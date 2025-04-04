651bc7a04f151ccada1fa989f29c66a081d76cd6
Iankennethgacusan <ganadoian31@gmail.com>
2025-04-04 22:47:56 +0800

    Create Act1_Arrays.py

---
+++ Gacusan_CS1A/Act1_Arrays.py
@@ -0,0 +1,17 @@
+// Create an array called favSingers and store your three favorite singers in it.
+const favSingers = ["Taylor Swift", "Arthur Nery", "Rey Valera"];
+
+// Log the first singer in the array using array index notation.
+console.log(favSingers[0]);
+
+// Create an array called favNumbers and store four of your favorite numbers.
+const favNumbers = [7, 13, 21, 37];
+
+// Create an array named mixedArr and store different data types: ["string", ["otherarray"], 123, true].
+const mixedArr = ["string", ["otherarray"], 123, true];
+
+// Access and log each item in the mixedArr using array index notation.
+console.log(mixedArr[0]); // "string"
+console.log(mixedArr[1]); // ["otherarray"]
+console.log(mixedArr[2]); // 123
+console.log(mixedArr[3]); // true
